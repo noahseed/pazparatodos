8a7331f054a668324969dbc05855e0ec86289684
Sébastien RONDEAU-CAMEIRA <2760742+noahseed@users.noreply.github.com>
2019-01-20 15:40:55 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A PAZ para todos... Sim, TODOS.">
+        <meta name="keywords" content="paz, vegano, vegan, ética, ambiente, saúde, vitamina, b12,
+        vegetariano, ecologia, proteína, vegetal, animal, queijo, leite, leiteria, ovo, carne,
+        vivissecção, cálcio, couro, lã, seda, mel, ferro, heme, zoológico, circo">
+        <meta name="robots" content="all">
+        <meta name="Content-language" content="portuguese">
+        <meta name="Rating" content="General">
+        <meta name="revisit-after" content="1 month">
+        <meta name="author" content="Sébastien RONDEAU-CAMEIRA">
+        <title>PAZ para TODOS</title>
+        <link rel="stylesheet" href="https://paixpourtous.com/css/style.css">
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+        <link rel="icon" type="image/png" href="favicon.png">
+        <link rel="alternate" hreflang="en" href="https://peace4everyone.info/">
+        <link rel="alternate" hreflang="fr" href="https://paixpourtous.com/">
+        <link rel="alternate" hreflang="it" href="https://pacepertutti.com/">
+        <link rel="alternate" hreflang="x-default" href="https://paixpourtous.com/">
+        <meta property="og:title" content="PAZ para TODOS">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://pazparatodos.com/">
+        <meta property="og:image" content="images/aum.png">
+        <meta property="og:description" content="A PAZ para todos... Sim, TODOS.">
+        <meta property="og:locale" content="pt_PT">
+        <meta property="og:site_name" content="PAZ para TODOS">
+    </head>
+    <body>
+        <main id="text">
+            <h1>A PAZ no MUNDO começa no <u>TEU</u> prato.</h1>
+            <form action="https://www.google.pt/search" method="GET">
+                <input type="hidden" value="vegano vegan" name="q">
+                <p>GO
+                    <input type="submit" value="VEGAN !" class="btn1">
+                </p>
+            </form>
+            <br>
+            <h2>Informa-te...</h2>
+            <h3>O teu melhor voto, é o teu cartão de crédito.</h3>
+            <form action="https://www.google.pt/search" method="GET">
+                <input type="hidden" value="vegano vegan" name="q">
+                <p>GO
+                    <input type="submit" value="VEGANO !!!" class="btn2">
+                </p>
+            </form>
+        </main>
+        <footer id="barre_langues">
+            <nav>
+                <a href="https://peace4everyone.info/"><img src="images/drap_en.gif" width="35" height="25" alt="English"></a>
+                <a href="https://paixpourtous.com/"><img src="images/drap_fr.gif" width="35" height="25" alt="Français"></a>
+                <a href="https://pacepertutti.com/"><img src="images/drap_it.gif" width="35" height="25" alt="Italiano"></a>
+            </nav>
+        </footer>
+    </body>
+</html>
